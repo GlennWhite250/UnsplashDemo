45ff51f8722476d18f45c104225f391b544b4104
Glenn White <Glennwhite5@gmail.com>
2023-02-16 00:02:47 -0500

    Laid the ground work for Dagger-Hilt for DI and Room for local storage

--- app/src/main/java/com/example/unsplashdemo/utils/Constants.kt
+++ app/src/main/java/com/example/unsplashdemo/utils/Constants.kt
@@ -2,4 +2,8 @@ package com.example.unsplashdemo.utils
 
 object Constants {
     const val UNSPLASH_IMAGE_TABLE = "unsplash_image_table"
+    const val UNSPLASH_REMOTE_KEYS_TABLE = "unsplash_remote_keys_table"
+    const val UNSPLASH_DATABASE = "unsplash_database"
+
+    const val BASE_URL = "https://api.unsplash.com"
 }
