b79799a05f0003367016bed91ea49599274d6274
Glenn White <Glennwhite5@gmail.com>
2023-02-27 08:13:48 -0500

    Updated the API_KEY to use a constant

---
+++ app/src/main/java/com/example/unsplashdemo/screens/common/ListContent.kt
@@ -0,0 +1,161 @@
+package com.example.paging3demo.screens.common
+
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.items
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.example.unsplashdemo.R
+import com.example.unsplashdemo.model.UnsplashImage
+import com.example.unsplashdemo.model.Urls
+import com.example.unsplashdemo.model.User
+import com.example.unsplashdemo.model.UserLinks
+
+
+@ExperimentalCoilApi
+@Composable
+fun ListContent(items: LazyPagingItems<UnsplashImage>) {
+    Log.d("Error", items.loadState.toString())
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(all = 12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(
+            items = items,
+            key = { unsplashImage ->
+                unsplashImage.id
+            }
+        ) { unsplashImage ->
+            unsplashImage?.let { UnsplashItem(unsplashImage = it) }
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun UnsplashItem(unsplashImage: UnsplashImage) {
+    val painter = rememberAsyncImagePainter(model = ImageRequest.Builder(LocalContext.current)
+        .error(R.drawable.ic_placeholder)
+        .placeholder(R.drawable.ic_placeholder)
+        .crossfade(durationMillis = 1000))
+    val context = LocalContext.current
+    Box(
+        modifier = Modifier
+            .clickable {
+                val browserIntent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://unsplash.com/@${unsplashImage.user.username}?utm_source=DemoApp&utm_medium=referral")
+                )
+                startActivity(context, browserIntent, null)
+            }
+            .height(300.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painter,
+            contentDescription = "Unsplash Image",
+            contentScale = ContentScale.Crop
+        )
+        Surface(
+            modifier = Modifier
+                .height(40.dp)
+                .fillMaxWidth(),
+            color = Color.Black
+        ) {}
+        Row(
+            modifier = Modifier
+                .height(40.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 6.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = buildAnnotatedString {
+                append("photo by ")
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)){
+                    append(unsplashImage.user.username)
+                }
+                append(" on Unsplash")
+            },
+            color = Color.White,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis)
+            LikeCounter(
+                modifier = Modifier.weight(3f),
+                painter = painterResource(id = R.drawable.ic_heart),
+                likes = "${unsplashImage.likes}"
+            )
+        }
+    }
+}
+
+@Composable
+fun LikeCounter(
+    modifier: Modifier,
+    painter: Painter,
+    likes: String
+) {
+    Row(
+        modifier = modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End
+    ) {
+        Icon(
+            painter = painter,
+            contentDescription = "Heart Icon",
+            tint = Color.Red
+        )
+        Divider(modifier = Modifier.width(6.dp))
+        Text(
+            text = likes,
+            color = Color.White,
+            //fontSize = MaterialTheme.typography.bodySmall,
+            fontWeight = FontWeight.Bold,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+@Preview
+fun UnsplashImagePreview() {
+    UnsplashItem(
+        unsplashImage = UnsplashImage(
+            id = "1",
+            urls = Urls(regular = ""),
+            likes = 100,
+            user = User(username = "Glenn White", userLinks = UserLinks(html = ""))
+        )
+    )
+}
---
+++ app/src/main/java/com/example/unsplashdemo/screens/home/HomeTopBar.kt
@@ -0,0 +1,39 @@
+package com.example.unsplashdemo.screens.home
+
+import androidx.compose.material3.*
+
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopBar(
+    onSearchClicked: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Home",
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        },
+        actions = {
+            IconButton(onClick = onSearchClicked) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@Preview
+fun HomeTopBarPreview() {
+    HomeTopBar {}
+}
